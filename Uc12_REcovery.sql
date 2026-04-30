e6c98cca7d9a3c30d0afed3a08d74cc0f7af30ea
Ayush singh <ayushsingh02082004@gmail.com>
2026-04-30 09:21:30 +0530

    updated uc 12

--- Uc12_REcovery.sql
+++ Uc12_REcovery.sql
@@ -3,5 +3,4 @@ DROP DATABASE "Multi-Tenant_ Event_Analytics_System";
 CREATE DATABASE "Multi-Tenant_ Event_Analytics_System";
 
 # Step 2: Run the Restore Command
-# Note: You may be prompted to type your postgres password.
 "D:\softwares\psql\bin\pg_restore.exe" -U postgres -d analytics_db -1 -j 4 "D:\backups\postgres\Multi-Tenant_ Event_Analytics_System_20260418_1548.dump"
